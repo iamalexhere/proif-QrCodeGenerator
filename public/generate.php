68513fbc9b1bb6e7c0981f4318646a0226d5adc6
doniandrian <doniandrian.talenta@gmail.com>
2024-09-11 23:39:49 +0700

    generate qr done

--- public/generate.php
+++ public/generate.php
@@ -5,7 +5,9 @@ require '../vendor/autoload.php';
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Logo\Logo;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
+
 use Endroid\QrCode\Builder\Builder;
 
 
@@ -15,16 +17,30 @@ if (isset($_POST['url-input']) && !empty($_POST['url-input'])) {
     $qrCode->setSize(300); 
     $qrCode->setMargin(10); 
     // buat logo
-    $logo = Logo::create("../public/images/logo_small.png")
-        ->setResizeToWidth(50);
+    $qrCode    
+    ->setErrorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->setBackgroundColor(new \Endroid\QrCode\Color\Color(255, 255, 255)); // Latar belakang putih
+
+
+
+    $logo = Logo::create("../public/images/Logo.jpg")
+        ->setResizeToWidth(70)
+        ->setPunchoutBackground(true);
+
+
      
     $writer = new PngWriter;
 
     $result = $writer->write($qrCode, logo:$logo);
 
     header("Content-Type: " . $result->getMimeType());
-    echo $result->getString();
+    $imageData  = $result->getString();
+    $base64Image = base64_encode($imageData); //ubah ke base64 agar bisa ditampilkan di HTML
+
+    echo $base64Image;
 } else {
     echo "Error: No URL provided!";
+    //kirim error jika tidak ada URL yang diberikan
+    
     exit; // Stop jika tidak ada URL diberikan
 }
--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <title>IF Unpar QR Code Generator</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -14,24 +14,22 @@
 </head>
 <body>
 
-<!-- HEADER: MENU + HEROE SECTION -->
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand">
-            <img src='../public/images/Logo.jpg' width="50" height="50">
+            <img src='images/Logo.jpg' width="50" height="50">
         </a>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">IF UNPAR Qr Code Generator</a>
+                    <a class="nav-link" href="#">IF UNPAR QR Code Generator</a>
                 </li>
             </ul>
         </div>
     </nav>
 </header>
 
-<!-- CONTENT -->
 
 <section class="h-100">
     <div class="container">
@@ -46,10 +44,10 @@
                     </ul>
                 </nav>
                 <div class="container border w-full">
-                    <form method="post" action="generate.php">
+                    <form id="qrForm" method="post" action="generate.php">
                         <div class="form-group mt-3">
                             <label for="url-input">URL</label>
-                            <input type="text" name="url-input" class="form-control" id="url-input" placeholder="https://yoururlhere.com">
+                            <input type="text" name="url-input" class="form-control" id="url-input" placeholder="Tulis URL anda di sini">
                         </div>
                         <div class="d-flex justify-content-center mt-3">
                             <button type="submit" class="btn btn-primary btn-lg">Generate QR Code</button>
@@ -58,8 +56,10 @@
                 </div>
             </div>
             <div class="col-4">
+            <h3 class="text-center">Output QR Code</h3>
+
                 <div class="d-flex justify-content-center mt-3">
-                    <img src='../public/images/qr_placeholder.svg'>
+                    <img id="qrImage" src='images/qr_placeholder.svg'>
                 </div>
                 
                 
@@ -87,3 +87,25 @@
 
 </body>
 </html>
+<script>
+    document.getElementById('qrForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Mencegah pengiriman formulir secara default
+        if(document.getElementById('url-input').value === '') {
+            alert('URL tidak boleh kosong!');
+            return;
+        }
+
+        const formData = new FormData(this);
+        fetch('generate.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('qrImage').src = 'data:image/png;base64,' + data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+</script>
