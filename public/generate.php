efdb02c957895547dd95cfd64ba00204ac6e72ea
doniandrian <doniandrian.talenta@gmail.com>
2024-09-19 14:26:40 +0700

    ipd

--- public/generate.php
+++ public/generate.php
@@ -12,9 +12,12 @@ header('Content-Type: application/json'); // Set header as JSON
 try{
     //periksa apakah vendor autoload sudah di load
     if (!class_exists('Endroid\QrCode\QrCode')) {
+        echo json_encode(['error' => 'Endroid\QrCode\QrCode class not found. Make sure you run composer install']);
         throw new Exception('Endroid\QrCode\QrCode class not found. Make sure you run composer install');
+    }else{
+        echo json_encode(['error' => 'Endroid\QrCode\QrCode class found.']);
     }
-    
+
 
     if (isset($_POST['url-input']) && !empty($_POST['url-input'])) {
         $text = $_POST['url-input'];
