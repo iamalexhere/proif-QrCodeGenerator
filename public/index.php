73b8720cb5d783038f769eca627fc1a02d183e3b
gharlanws <106369102+gharlanws@users.noreply.github.com>
2024-09-29 00:06:39 +0700

    revisi ui
    hapus beberapa class dan css yg ga perlu

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>IF Unpar QR Code Generator</title>
     <meta name="title" content="IF Unpar QR Code Generator">
-    <meta name="description" content="Website ini untuk membuat QR Code IF Unpar">
+    <meta name="description" content="Website untuk membuat QR Code IF Unpar">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:image" content="https://qrcode.ifunpar.id/images/logoif.png">
     <!-- <meta property="og:image:width" content="width_in_pixels">
@@ -17,50 +17,35 @@
 <body>
     <header>
         <nav class="navbar">
-            <a class="navbar-brand">
-                <img src='images/logoif.png' >
-            </a>
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link">QR Code Generator</a>
-                    </li>
-                </ul>
-            </div>
+            <img src='images/logoif.png' >
+            <a class="nav-link">QR Code Generator</a>
         </nav>
     </header>
     <section>
         <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-8">
-                    <div class="container border pb-3">
-                        <form id="qrForm" method="post" action="generate.php">
-                            <div class="form-group mt-3">
-                                <label class="url-input" for="url-input">URL</label>
-                                <input type="url" name="url-input" class="form-control" id="url-input"
-                                    placeholder="Tulis URL anda di sini">
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="use-logo" name="use-logo"
-                                    value="yes" checked>
-                                <label class="form-check-label" for="use-logo">Gunakan logo IF UNPAR</label>
-                            </div>
-                            <div class="d-flex justify-content-center mt-3">
-                                <button type="submit" class="btn btn-primary btn-lg">Generate QR Code</button>
-                            </div>
-                        </form>
-                    </div>
-
+            <div class="content">
+                <div class="inputsection">
+                    <form id="qrForm" method="post" action="generate.php">
+                        <div class="url-input" for="url-input">URL</div>
+                        <div>
+                            <input type="url" name="url-input" class="form-control" placeholder="Tulis URL anda di sini">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" name="use-logo"value="yes" checked>Gunakan logo IF UNPAR
+                        </div>
+                        <div class="form-submit"> 
+                            <button type="submit" class="btn">Generate QR Code</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="col-12 col-md-4">
-                    <h3 class="text-center">Output QR Code</h3>
-                    <div class=" d-flex justify-content-center mt-3">
+                <div class="outputsection">
+                    <h3>Output QR Code</h3>
+                    <div>
                         <img id="qrImage" src=''>
                     </div>
-                    <div class="d-flex justify-content-center mt-3">
-                        <a id="download-link" class="btn disabled d-flex">
-                            <img src="images/download.svg" alt="Icon" class="mr-2">
-                            Download PNG
+                    <div>
+                        <a id="download-link" class="btn">
+                            <img src="images/download.svg" alt="Icon">Download PNG
                         </a>
                     </div>
                 </div>
